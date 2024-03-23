2f921f057070bed1d938dbc2d2de156fa1015ef4
Ashim Koirala <ashimkoirala01@gmail.com>
2024-03-23 20:58:54 +0545

    node/express.js

--- node/express.js
+++ node/express.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Hello, World!");
+//in localhost:xxxx/
+app.get("/", (req, res) => {
+  res.send("home page");
 });
 
+//in localhost:xxxx/hello
+
+app.get("/hello", (req, res) => {
+  res.send("Hello page");
+});
+
+//xxxx=3000 in this case
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000/");
 });
